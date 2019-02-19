9a7122734a8d7eb77db45070d92e784090ade078
Jedi18 <targetakhil@gmail.com>
2019-02-19 10:41:46 +0530

    Inital commit

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class Ball : MonoBehaviour {
+
+    public SpriteRenderer spriteRenderer;
+    public Sprite sprite;
+
+    public float gravity = -1;
+    public float bounceHeight = 10;
+    public float speed = 0.01f;
+    public float speedX = 2f;
+
+    float velocityY = 0;
+    int directionX = 1;
+
+	// Use this for initialization
+	void Start () {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        velocityY = Mathf.Sqrt(-2 * gravity * bounceHeight);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        // Calculating Y velocity
+        velocityY += gravity * Time.deltaTime;
+        float velY = velocityY * speed;
+
+        // Calculating X velocity
+        float velX = speedX * directionX * speed;
+
+        Vector3 finalVelocity = new Vector3(velX,velY);
+
+        transform.Translate(finalVelocity);
+	}
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            velocityY = Mathf.Sqrt(-2 * gravity * bounceHeight);
+        }
+    }
+}
---
+++ Assets/Scripts/Setup.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Setup : MonoBehaviour {
+
+    Camera camera;
+    public GameObject boundry;
+
+    // Hardcoded values for left and right
+    float scaleX = 145.5721f;
+    float scaleY = 62.25f;
+
+	// Use this for initialization
+	void Start () {
+        camera = Camera.main;
+        InstantiateColliders();
+	}
+
+    void InstantiateColliders()
+    {
+        float distY = camera.orthographicSize;
+        float distX = distY * camera.aspect;
+
+        // Left
+        GameObject tra1 = (GameObject)Instantiate(boundry, new Vector2(-distX - 0.1f, 0), gameObject.transform.rotation);
+        tra1.transform.localScale = new Vector2(tra1.transform.localScale.x, scaleY);
+
+        // Right
+        GameObject tra2 = (GameObject)Instantiate(boundry, new Vector2(distX + 0.1f, 0), gameObject.transform.rotation);
+        tra2.transform.localScale = new Vector2(tra2.transform.localScale.x, scaleY);
+
+        // Up
+        GameObject tra3 = (GameObject)Instantiate(boundry, new Vector2(0, distY + 0.1f), gameObject.transform.rotation);
+        tra3.transform.localScale = new Vector2(scaleX, tra3.transform.localScale.y);
+
+        // Down
+        GameObject tra4 = (GameObject)Instantiate(boundry, new Vector2(0, -distY - 0.1f), gameObject.transform.rotation);
+        tra4.transform.localScale = new Vector2(scaleX, tra4.transform.localScale.y);
+
+        Debug.Log(distY);
+        Debug.Log(distX);
+    }
+}
